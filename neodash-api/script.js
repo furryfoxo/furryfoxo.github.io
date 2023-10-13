d160ac70e7fef3055254e89d9424f34ffdaf51f9
Swarkin <102416174+Swarkin@users.noreply.github.com>
2023-10-13 18:39:07 +0200

    Minor refactor

--- neodash-api/script.js
+++ neodash-api/script.js
@@ -1,37 +1,43 @@
-console.log('Hello World!');
-
 const ADDRESS = 'https://indirectly-fast-gorilla.ngrok-free.app'
+
 let btn = document.getElementById('btn');
 let tableBody = document.getElementById('tableBody')
 
+
 btn.addEventListener('click', function () {
-	let endpoint = '/hall-of-fame'
 	let http = new XMLHttpRequest();
-	console.log('Requesting', endpoint)
+	let endpoint = '/hall-of-fame'
+
 	http.open('GET', ADDRESS+endpoint)
+	http.setRequestHeader('ngrok-skip-browser-warning', '*')
 	http.send()
+
 	http.onreadystatechange = () => {
 		if (http.readyState == http.DONE && http.status == 200) {
+			while (tableBody.firstChild) {tableBody.firstChild.remove()}
+
 			let data = JSON.parse(http.responseText)
-			while (tableBody.firstChild) {
-				tableBody.firstChild.remove()
-			}
 			let users = data.users
 			let scores = data.scores
+
 			for (let i = 0; i < scores.length; i++) {
 				let row = document.createElement('tr')
-				if (i == 0) {row.className = 'first'}
-				else if (i == 1) {row.className = 'second'}
-				else if (i == 2) {row.className = 'third'}
 				let rank = document.createElement('td')
 				let user = document.createElement('td')
 				let score = document.createElement('td')
+
+				if (i == 0) {row.className = 'first'}
+				else if (i == 1) {row.className = 'second'}
+				else if (i == 2) {row.className = 'third'}
+
 				row.appendChild(rank)
 				row.appendChild(user)
 				row.appendChild(score)
+
 				rank.textContent = i+1
 				user.textContent = users[i]
 				score.textContent = scores[i]
+
 				tableBody.appendChild(row)
 			}
 		}
